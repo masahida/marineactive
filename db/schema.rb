a098f0cca0cc76e0796ec9885c011ebcf14a7ac3
github hidamasa <mscreate0427@gmail.com>
2018-09-18 12:30:52 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180915150028) do
+ActiveRecord::Schema.define(version: 2018_09_17_151743) do
 
-  create_table "posts", force: :cascade do |t|
-    t.text     "content"
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "image_name"
-    t.string   "password"
+    t.string "image_name"
+    t.string "password_digest"
   end
 
 end
